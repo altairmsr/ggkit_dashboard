236927b7a8480d775e78e7c126b0f20f38ef3257
AltairSR <34604370+weirdmansmr@users.noreply.github.com>
2024-09-16 14:54:33 +0300

    Update main.jsx

--- ggkit-dashboard/src/main.jsx
+++ ggkit-dashboard/src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Students from "./pages/Students.jsx";
 import Groups from "./pages/Groups.jsx";
 import "./App.css";
 
 const router = createBrowserRouter([
-	createRoutesFromElements(
-	<Route path="/" element={<Students />} />
-      	<Route path="groups" element={<Groups />} />
-	
-  )
+	{
+		path: "/",
+		element: <Students />,
+	},
+	{
+		path: "/groups",
+		element: <Groups />,
+	},
 ]);
 
 createRoot(document.getElementById("root")).render(
