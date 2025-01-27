02bed8f9d802a8a318c2679998333ce66f706549
msajhanov <msajhanov@team.amocrm.com>
2025-01-28 01:27:13 +0300

    UPD info 2025

--- src/data/groupList.js
+++ src/data/groupList.js
@@ -1,30 +1,30 @@
 const groupList = [
 	{
 		id: 0,
-		group: "Э1.23-21",
+		group: "О5.24-12",
 		img: "gold",
 		pos: "gold",
 	},
 	{
 		id: 1,
-		group: "О5.24-12",
+		group: "О5.24-11",
 		img: "silver",
 		pos: "silver",
 	},
 	{
 		id: 2,
-		group: "О5.24-11",
+		group: "О6.24-11",
 		img: "bronze",
 		pos: "bronze",
 	},
 	{
 		id: 3,
-		group: "О1.23-23",
+		group: "Э1.23-21",
 		pos: "regular",
 	},
 	{
 		id: 4,
-		group: "О6.24-11",
+		group: "И1.23-21",
 		pos: "regular",
 	},
 ];
