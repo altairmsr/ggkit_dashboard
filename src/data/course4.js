b5f1f1561dae75c3579c799322866cdb1ae40ee8
msajhanov <msajhanov@team.amocrm.com>
2025-02-25 22:41:41 +0300

    UPD info 25.02.2025

--- src/data/course4.js
+++ src/data/course4.js
@@ -2,28 +2,29 @@ const course4 = [
 	{
 		id: 0,
 		group: "С1.21-41",
-		name: "Асхабов И.",
+		name: "Вахаев З.",
+		
 		img: "gold",
 		pos: "gold",
 	},
 	{
 		id: 1,
 		group: "С1.21-41",
-		name: "Вахаев З.",
+		name: "Асхабов И.",
 		img: "silver",
 		pos: "silver",
 	},
 	{
 		id: 2,
 		group: "С1.21-41",
-		name: "Хасханов А.",
+		name: "Асхабов М.",
 		img: "bronze",
 		pos: "bronze",
 	},
 	{
 		id: 3,
 		group: "С1.21-41",
-		name: "Асхабов М.",
+		name: "Хасханов А.",
 		pos: "regular",
 	},
 	{
@@ -35,13 +36,13 @@ const course4 = [
 	{
 		id: 5,
 		group: "С1.21-41",
-		name: "Андиев М.",
+		name: "Абдулаев И.",
 		pos: "regular",
 	},
 	{
 		id: 6,
 		group: "С1.21-41",
-		name: "Абдулаев И.",
+		name: "Андиев М.",
 		pos: "regular",
 	},
 	{
@@ -53,13 +54,13 @@ const course4 = [
 	{
 		id: 8,
 		group: "С1.21-41",
-		name: "Мутуев И.",
+		name: "Мальцагов А.",
 		pos: "regular",
 	},
 	{
 		id: 9,
 		group: "С1.21-41",
-		name: "Мальцагов А.",
+		name: "Мутуев И.",
 		pos: "regular",
 	},
 ];
