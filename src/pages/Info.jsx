1f582b3c00357c772ca0394c52db12f1a2b2301d
weirdmansmr <maga.saykhanov95@gmail.com>
2025-06-10 00:23:43 +0300

    for the ABILYMPICS!!!

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -7,9 +7,9 @@ const Info = () => {
       style={{
         textAlign: "center",
         padding: "0 30px",
-        fontSize: "2em",
+        fontSize: "2.5em",
         color: "#111",
-        backgroundColor: "#F7F3D6",
+        backgroundImage: "linear-gradient(135deg, #4568DC, #B06AB3)",
         height: "100vh",
         display: "flex",
         justifyContent: "center",
@@ -18,7 +18,7 @@ const Info = () => {
     >
       <h3>
         Приветствуем участников отборочного этапа национального чемпионата
-        Абилимпикс 2025
+        <span style={{ color: "white" }}> Абилимпикс 2025</span>
       </h3>
     </div>
   );
