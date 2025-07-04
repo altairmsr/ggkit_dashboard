048ba1f32934bf8d7bebb4278de0dde41149bc2c
WeirdmanSMR <maga.saykhanov95@gmail.com>
2025-07-04 17:01:37 +0300

    added promo

--- src/pages/Announcement/Announcement.jsx
+++ src/pages/Announcement/Announcement.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import style from "./Anno.module.scss";
 
 const Announcement = () => {
-	return <div className={style.announce}></div>;
+    return (
+        <>
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                style={{
+                    position: "fixed",
+                    right: 0,
+                    bottom: 0,
+                    width: "100vw",
+                    height: "100vh",
+                    objectFit: "cover",
+                    zIndex: -1,
+                }}
+            >
+                <source src="/assets/GGKIT.mp4" type="video/mp4" />
+                Ваш браузер не поддерживает видео.
+            </video>
+            {/* <div className={style.announce}>
+            </div> */}
+        </>
+    );
 };
 
 export default Announcement;
